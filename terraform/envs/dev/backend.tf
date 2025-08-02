2e815be891fa49fe8462a7590b483ad498f4cfe3
hieu.nguyen <samelnguyen08@gmail.com>
2025-08-02 14:49:57 +0700

    cicd: update correct backend s3 for dev and uat

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aj3-aws-infra-terraform-state-dev"
+    bucket = "aj3-aws-infra-terraform-state"
     key    = "dev/terraform.tfstate"
     region = "ap-southeast-1"
   
--- terraform/envs/uat/backend.tf
+++ terraform/envs/uat/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "aj3-aws-infra-terraform-state-uat"
-    key    = "dev/terraform.tfstate"
+    bucket = "aj3-aws-infra-terraform-state"
+    key    = "uat/terraform.tfstate"
     region = "ap-southeast-1"
   
   }
