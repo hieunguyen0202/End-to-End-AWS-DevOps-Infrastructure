276abc90347b0d29b3a6493281f5878523347569
hieu.nguyen <samelnguyen08@gmail.com>
2025-08-03 20:20:52 +0700

    fix: update new ansible configure

--- terraform/modules/bastion/main.tf
+++ terraform/modules/bastion/main.tf
@@ -5,6 +5,13 @@ locals {
 }
 
 
+resource "local_sensitive_file" "private_key" {
+  filename        = "${path.module}/keypair/ansible.pem"
+  content         = file("${path.module}/keypair/aj3-aws-infra-bastion-key")
+  file_permission = "0400"
+}
+
+
 resource "aws_key_pair" "bastion_key" {
   key_name   = var.key_name
   public_key = file("${path.module}/keypair/aj3-aws-infra-bastion-key.pub")
@@ -19,10 +26,21 @@ resource "aws_instance" "bastion" {
   key_name                    = aws_key_pair.bastion_key.key_name
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update && sudo apt install mysql-client -y
-              EOF
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y software-properties-common",
+      "sudo apt-add-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install -y ansible"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("${path.module}/keypair/aj3-aws-infra-bastion-key")
+      host        = self.public_ip
+    }
+  }
 
   root_block_device {
     volume_size = var.volume_size
