fd08ac3717861f26644d241b29d2423cd6e2ee56
hieu.nguyen <hieu.nguyen@upskills.com>
2025-08-05 13:20:52 +0700

    fix: update allow port 80 for SG

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -82,6 +82,13 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["10.0.10.0/24", "10.0.20.0/24"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.10.0/24", "10.0.20.0/24"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
